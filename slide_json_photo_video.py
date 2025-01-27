526682185aa6ba70d2f4af00772ca96f97c9d78f
Luiz Guilherme de M. Ventura <ziul_ventura@hotmail.com>
2025-01-27 23:42:42 +0100

    Generating JSON file as desired in this first approach, also extracting video durations.

--- slide_json_photo_video.py
+++ slide_json_photo_video.py
@@ -11,18 +11,19 @@ Created on Sun Jan 26 16:44:12 2025
 
 import datetime as dt
 import json
-import numpy as np
+#import numpy as np
 import pandas as pd
 from os import path, listdir
 import re
 import exif
 import ffmpeg
 
+
 img_formats = ('.jpg')
 vid_formats = ('.mp4')
 
 data = {'folder': ['/media/luiz/HDp1/Câmeras/EOSR6mk2/20250112/100EOSR6',
-                   '/media/luiz/HDp1/Câmeras/EOSR6mk2/20250112/100EOSR6',
+                   '/media/luiz/HDp1/Câmeras/EOSR6mk2/20250112/101EOSR6',
                    '/media/luiz/HDp1/Celular/MiNote10/20250112'],
         'datetime photo': ['exif',
                            'exif',
@@ -104,3 +105,85 @@ data_all = {'path': all_paths,
             'dt_with_offset': all_dt_w_offset}
 
 df_all = pd.DataFrame(data_all)
+
+#%%
+
+import ffmpeg
+
+# standard values. After the file is generated, they can be tweeked for each photo or video
+duration_photo = 3
+zoom = [1.0, 1.0]
+pan = [
+        {"x": 50, "y": 50},
+        {"x": 50, "y": 50}
+      ]
+muted_video = True
+
+def get_video_duration(video_path):
+    try:
+        # Usa o ffmpeg para obter informações sobre o vídeo
+        probe = ffmpeg.probe(video_path)
+        
+        # Encontra o stream de vídeo
+        video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+        
+        if video_stream is not None:
+            # Extrai a duração do vídeo
+            duration = float(video_stream['duration'])
+            return duration
+        else:
+            print("Nenhum stream de vídeo encontrado.")
+            return None
+    except ffmpeg.Error as e:
+        print(f"Erro ao processar o vídeo: {e.stderr}")
+        return None
+
+datetime_init = dt.datetime(2024, 12, 17, 3, 0, 0)
+datetime_final = dt.datetime(2024, 12, 18, 3, 0, 0)
+
+df_filt = df_all[(df_all['dt_with_offset'] >= datetime_init) & (df_all['dt_with_offset'] <= datetime_final)]
+
+df_sorted = df_filt.sort_values(by='dt_with_offset').reset_index()
+
+file_paths = []
+durations = []
+zooms = []
+pans = []
+muted = []
+for idx, row in df_sorted.iterrows():
+    fp = path.join(row.path, row.file)
+    file_paths.append(fp)
+    if row.category == 'image':
+        durations.append(duration_photo)
+        zooms.append(zoom)
+        pans.append(pan)
+        muted.append(None)
+    elif row.category == 'video':
+        duration = get_video_duration(fp)
+        duration = round(duration)  # optional
+        durations.append(duration)
+        zooms.append(None)
+        pans.append(None)
+        muted.append(muted_video)
+
+data_out = {"file": file_paths,
+            "duration": durations,
+            "zoom": zooms,
+            "pan": pans,
+            "muted": muted}
+
+df_out = pd.DataFrame(data_out)
+
+# Exportando para JSON com orientação 'records'
+# json_str = df_out.to_json(orient='records', indent=4)
+
+# Converter DataFrame para lista de dicionários
+dados = df_out.to_dict(orient='records')
+
+# Exportar para JSON sem escapar caracteres especiais
+json_str = json.dumps(dados, ensure_ascii=False, indent=4)
+
+# Salvando em um arquivo
+with open('slides.json', 'w') as f:
+    f.write(json_str)
+    
